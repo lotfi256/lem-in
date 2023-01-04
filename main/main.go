41cb48787113f8936ce8779f65a540fd2e0fe08f
antoniosakkal <antoniosakkal1998@gmail.com>
2023-01-04 22:44:00 +0000

    fixed links extraction

--- main/main.go
+++ main/main.go
@@ -13,9 +13,8 @@ func main() {
 	data, _ := os.ReadFile(filename)
 	content := strings.Split(string(data), "\n")
 	MyMap, index := lemin.ValidateRooms(content)
+
 	MyMap = lemin.ValidateLinks(content[index:], MyMap)
 
-	for k, v := range MyMap {
-		fmt.Println("key:", k, "value:", v)
-	}
+	fmt.Println(MyMap)
 }
