3ea09344d27783ce403ad432aa919898af49956b
antoniosakkal <antoniosakkal1998@gmail.com>
2023-01-16 12:43:53 +0000

    adjustement

--- initialization.go
+++ initialization.go
@@ -21,6 +21,8 @@ type LL struct {
 
 var Result LL
 
+// STEP 1: draw the colony from MyMap
+// I will extract and organize the data from the map:
 func AdjustMap(MyMap *Map) {
 
 	//Starting room is the first entry in the LinkedList
@@ -28,15 +30,16 @@ func AdjustMap(MyMap *Map) {
 		if key.Start {
 			//the starting point will always be Tail
 			//Head will move across the map
-			Result.Head, Result.Tail = key, key
+			Result.Tail = key
 		}
 		if key.End {
+			Result.Head = key
 			key.Parents = key.Children
 			key.Children = nil
 		}
 	}
-	for Result.Head.Children != nil {
-		LinkedList(Result.Head)
+	for Result.Tail.Children != nil {
+		LinkedList(Result.Tail)
 	}
 }
 
@@ -47,41 +50,18 @@ func AdjustMap(MyMap *Map) {
 func LinkedList(Node *Vertice) {
 	Node.Visited = true
 	for _, v := range Node.Children {
-		if !v.End {
+		if !v.End && !v.Visited {
 			v.Parents = append(v.Parents, Node)
 			for index, item := range v.Children {
 				if item.Name == Node.Name {
 					v.Children = append(v.Children[:index], v.Children[index+1:]...)
 				}
 			}
-			Result.Head = v
+		} else {
+			break
 		}
+		LinkedList(v)
 	}
-}
-
-// //THIS SHOULD BE A RECURSIVE FUNCTION
-// for _, room := range Result.Head.Children {
-// 	room.Parents = append(room.Parents, Result.Head)
-// 	//End room has no children
-// 	if room.End {
-// 		room.Parents = room.Children
-// 		room.Children = nil
-// 	}
-// 	//remove Parent nodes from children
-// 	for i, element := range room.Children {
-// 		if Result.Head == element && i < len(room.Children) {
-// 			room.Children = append(room.Children[:i], room.Children[i+1:]...)
-// 		} else if Result.Head == element {
-// 			room.Children = room.Children[:i]
-// 		}
-// 	}
-
-// }
-
-// STEP 1: draw the colony from MyMap
-// I will extract and organize the data from the map:
-func DrawMap(linkedlist *LL) {
-	// will link from
 }
 
 //STEP 2: initialize a BFS or DFS (pick the quickest method)
